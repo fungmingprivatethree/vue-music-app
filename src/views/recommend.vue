e36531c5214a36b7dd176261cb9f48f8015f9e49
fungmingprivatethree <80408226+fungmingprivatethree@users.noreply.github.com>
2021-08-07 21:27:58 +0800

    second commit 7/8/2021 21:27

---
+++ src/views/recommend.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="recommend">
+    推薦頁面
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'recommend'
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
