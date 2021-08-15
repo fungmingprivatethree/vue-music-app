ce4689c7de2755bc9e46f005486da3754808a7fd
fungmingprivatethree <80408226+fungmingprivatethree@users.noreply.github.com>
2021-08-15 13:13:41 +0800

    3.9 move action in shortcut bar and it can quickly locate the relative position

--- src/components/base/index-list/use-shortcut.js
+++ src/components/base/index-list/use-shortcut.js
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 
 export default function useShortcut(props, groupRef) {
+    const ANCHOR_HEIGHT = 18
     const scrollRef = ref(null)
 
     const shortcutList = computed(() => {
@@ -9,19 +10,35 @@ export default function useShortcut(props, groupRef) {
         })
     })
 
+    const touch = {}
+
     function onShortcutTouchStart(e) {
         // we can store the value to target properties, make use of target object tp get the properties
         // :data-index="index" 將 index 綁定在 target's properties : data-index
         const anchordIndex = parseInt(e.target.dataset.index)
         // make use of betterScroll method : scrollToElement
         // get the related DOM object
-        const targetEl = groupRef.value.children[anchordIndex]
-        const scroll = scrollRef.value.scroll
-        scroll.scrollToElement(targetEl, 0)
+        touch.y1 = e.touches[0].pageY
+        // we can store the anchordIndex value in touch object. Then, it can be passed to cross function
+        touch.anchordIndex = anchordIndex
+
+        scrollTo(anchordIndex)
     }
 
     function onShortcutTouchMove(e) {
+        touch.y2 = e.touches[0].pageY
+        const delta = (touch.y2 - touch.y1) / ANCHOR_HEIGHT | 0
+        const anchorIndex = touch.anchordIndex + delta
 
+        scrollTo(anchorIndex)
+    }
+
+    function scrollTo(index) {
+        // prevent the width is over and throw error : annot read property 'offsetWidth' of undefined
+        index = Math.max(0, Math.min(shortcutList.value.length - 1, index))
+        const targetEl = groupRef.value.children[index]
+        const scroll = scrollRef.value.scroll
+        scroll.scrollToElement(targetEl, 0)
     }
 
     return {
